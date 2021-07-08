9dd1fda7dd2454a306c54c27c7a8ff9fa51de8d9
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2021-07-08 17:31:10 +0300

    Update RemoteChatServer.java

--- ServerMain/src/main/java/RemoteChatServer.java
+++ ServerMain/src/main/java/RemoteChatServer.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class RemoteChatServer implements Chat {
+public class RemoteChatServer implements Chat{
     MySQLClass sql = new MySQLClass();
     int countMessage;
     int countAuthorization;
@@ -15,6 +15,49 @@ public class RemoteChatServer implements Chat {
 
     private static List<String> jsonToRooms(String fileName) throws FileNotFoundException {
         return Arrays.asList(new Gson().fromJson(new FileReader(fileName), String[].class));
+    }
+
+    @Override
+    public String checkAuthorization(String login, String password) throws RemoteException {
+        try{
+            Map<String, String> map = sql.check();
+            if (map != null && !map.isEmpty()){
+                for (Map.Entry<String, String> entry : map.entrySet()){
+                    if (login.equals(entry.getKey())){
+                        if(!password.equals(entry.getValue())){
+                            System.out.println("incorrect password");
+                            return "incorrect password";
+                        }
+                        else {
+                            System.out.println("Ok");
+                            return "authorization is OK";
+                        }
+                    }
+                    else{
+                        sql.addAuthorization(countAuthorization, new User(login, password));
+                        incrementAuthorization();
+                        System.out.println("new registration");
+                        return "new registration";
+                    }
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void incrementMessage(){
+        countMessage++;
+    }
+    public void incrementAuthorization(){countAuthorization++;}
+
+    @Override
+    public String sendMessage(String message) throws RemoteException {
+        sql.addMessage(countMessage, message);
+        incrementMessage();
+        return message;
     }
 
     @Override
@@ -29,41 +72,4 @@ public class RemoteChatServer implements Chat {
             e.printStackTrace();
         }
     }
-
-    @Override
-    public String sendMessage(String message) throws RemoteException {
-        sql.addMessage(countMessage, message);
-        incrementMessage();
-        return message;
-    }
-
-    @Override
-    public void checkAuthorization(String login, String password) throws RemoteException {
-        try{
-            Map<String, String> map = sql.check();
-            if (map != null && !map.isEmpty()){
-                for (Map.Entry<String, String> entry : map.entrySet()){
-                    if (login.equals(entry.getKey())){
-                        if(!password.equals(entry.getValue())){
-                            System.out.println("incorrect password");
-                        }
-                        else {
-                            System.out.println("Ok");
-                        }
-                    }
-                }
-            }
-            else{
-                sql.addAuthorization(countAuthorization, new User(login, password));
-                incrementAuthorization();
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void incrementMessage(){
-        countMessage++;
-    }
-    public void incrementAuthorization(){countAuthorization++;}
 }
