19d9e12983d253f5f7e65a167fe389d84e3c2be7
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2021-07-12 17:49:26 +0300

    Update RemoteChatServer.java

--- ServerMain/src/main/java/RemoteChatServer.java
+++ ServerMain/src/main/java/RemoteChatServer.java
@@ -6,6 +6,7 @@ import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class RemoteChatServer implements Chat{
     MySQLClass sql = new MySQLClass();
@@ -13,8 +14,8 @@ public class RemoteChatServer implements Chat{
     int countAuthorization;
     private final static String JSON_FILE_NAME = "C:\\Users\\Philosoph\\IdeaProjects\\chat\\ServerMain\\src\\rooms.json";
 
-    private static List<String> jsonToRooms(String fileName) throws FileNotFoundException {
-        return Arrays.asList(new Gson().fromJson(new FileReader(fileName), String[].class));
+    private static List<Rooms> jsonToRooms(String fileName) throws FileNotFoundException {
+        return Arrays.asList(new Gson().fromJson(new FileReader(fileName), Rooms[].class));
     }
 
     @Override
@@ -41,12 +42,34 @@ public class RemoteChatServer implements Chat{
                     }
                 }
             }
+            else{
+                sql.addAuthorization(countAuthorization, new User(login, password));
+                incrementAuthorization();
+                System.out.println("new registration");
+                return "new registration";
+            }
 
         }catch (Exception e){
             e.printStackTrace();
         }
         return "";
     }
+    @Override
+    public List<String> chooseRoom() throws RemoteException {
+        List<Rooms> roomList = null;
+        List<String> list = new CopyOnWriteArrayList<>();
+        try {
+            roomList = jsonToRooms(JSON_FILE_NAME);
+            for (int i = 0; i < roomList.size(); i++) {
+                System.out.println((i + 1) + " " + roomList.get(i));
+                list.add(roomList.get(i).toString());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 
     public void incrementMessage(){
         countMessage++;
@@ -60,16 +83,5 @@ public class RemoteChatServer implements Chat{
         return message;
     }
 
-    @Override
-    public void chooseRoom() throws RemoteException {
-        List<String> roomList = null;
-        try {
-            roomList = jsonToRooms(JSON_FILE_NAME);
-            for (int i = 0; i < roomList.size(); i++) {
-                System.out.println((i + 1) + " " + roomList.get(i));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+
 }
