678107702bcb0a3a7b86ff388388ea450e37972e
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2021-09-09 14:38:11 +0300

    Update Client.java

--- ClientMain/src/Client.java
+++ ClientMain/src/Client.java
@@ -66,32 +66,37 @@ public class Client {
                 new Thread(() -> {
                     while (!Thread.currentThread().isInterrupted()){
                         try {
-                            Thread.sleep(500);
-                            checkLastMessage(finalI +1);
-//                            sendMessage(finalI +1, reader.readLine());
-                        } catch (IOException | InterruptedException e) {
+                            sendMessage(finalI +1, reader.readLine());
+                        } catch (IOException | NotBoundException e) {
                             e.printStackTrace();
-                        } catch (NotBoundException e) {
+                        }
+
+                    }
+                }).start();
+
+                new Thread(() -> {
+                    while (!Thread.currentThread().isInterrupted()){
+                        try {
+                            Thread.sleep(3000);
+                            checkLastMessage(finalI +1);
+                        } catch (InterruptedException | IOException | NotBoundException e) {
                             e.printStackTrace();
                         }
                     }
+
                 }).start();
-                sendMessage(finalI +1, reader.readLine());
-                writeMessage(finalI +1, reader.readLine());
+
             }
         }
     }
 
     public static void sendMessage(Integer room, String message) throws IOException, NotBoundException, RemoteException{
-        SimpleDateFormat date = new SimpleDateFormat("HH:mm");
-        System.out.println(user.getUserName() + " " + date.format(new Date()) + ": " + message);
-    }
-
-    public static void writeMessage(Integer room, String message) throws IOException, NotBoundException, RemoteException{
         final Registry registry = LocateRegistry.getRegistry("127.0.0.1",2732);
 
         Chat chat = (Chat) registry.lookup(UNIQUE_BINDING_NAME);
         String m = chat.sendMessage(room, message);
+        SimpleDateFormat date = new SimpleDateFormat("HH:mm");
+        System.out.println(user.getUserName() + " " + date.format(new Date()) + ": " + message);
     }
 
     public static void checkMessage(Integer room) throws IOException, NotBoundException, RemoteException{
@@ -100,6 +105,7 @@ public class Client {
         Chat chat = (Chat) registry.lookup(UNIQUE_BINDING_NAME);
         List<String> list = chat.checkMessage(room);
         checkList = list;
+
 
         int count = 0;
 
@@ -121,61 +127,18 @@ public class Client {
 
         Chat chat = (Chat) registry.lookup(UNIQUE_BINDING_NAME);
         List<String> list = chat.checkMessage(room);
-        System.out.println();
-        
+
+//        System.out.println();
+
         if(list.size() > checkList.size()){
             list.removeAll(checkList);
             for (int i = 0; i < list.size(); i++) {
                 System.out.println(list.get(i));
             }
         }
+
+
+//        System.out.println(list.get(list.size()-1));
     }
 
-
-//    public static void execute(Runnable task, long delaySec) {
-//        Timer timer = new Timer();
-//        TimerTask timerTask = new TimerTask() {
-//            @Override
-//            public void run() {
-//                task.run();
-//                timer.cancel();
-//            }
-//        };
-//        timer.schedule(timerTask, delaySec * 100);
-//    }
-
-//    static class ChatThread extends Thread{
-//        private final int room;
-//
-//        ChatThread (int room){
-//            this.room = room;
-//        }
-//
-//        public void run(){
-//
-//
-//            try{
-//                Thread.sleep(500);
-//                final Registry registry;
-//                Chat chat;
-//                try {
-//                    registry = LocateRegistry.getRegistry("127.0.0.1",2732);
-//                    chat = (Chat) registry.lookup(UNIQUE_BINDING_NAME);
-//                    List<String> list = chat.checkMessage(room);
-//                    System.out.println();
-//
-//                    for (int i = 0; i < list.size(); i++) {
-//                        System.out.println(list.get(list.size() - 1));
-//                    }
-//                } catch (RemoteException | NotBoundException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
-//            catch(InterruptedException e){
-//                System.out.println("Thread has been interrupted");
-//            }
-//
-//        }
-//    }
 }
