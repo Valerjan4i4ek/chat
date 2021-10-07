6bcc857dec2592f954e88680613491d416ab4a95
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2021-10-07 15:59:22 +0300

    Update Chat.java

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,13 +1,15 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Map;
 
 public interface Chat extends Remote {
-    String sendMessage (Integer room, String message) throws RemoteException;
+    String sendMessage (Integer room, String message, String user) throws RemoteException;
 
-    List<String> checkMessage(Integer room) throws RemoteException;
+    Map<List<String>, List<String>> checkMessage(Integer room) throws RemoteException;
 
     String checkAuthorization(String login, String password) throws RemoteException;
 
     List<String> chooseRoom() throws RemoteException;
+
 }
