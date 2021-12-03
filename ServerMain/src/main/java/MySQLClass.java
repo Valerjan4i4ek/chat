2b9bf11249884ad52d9bd46af553308bd11c08f1
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2021-12-03 17:19:37 +0200

    Update MySQLClass.java

--- ServerMain/src/main/java/MySQLClass.java
+++ ServerMain/src/main/java/MySQLClass.java
@@ -10,6 +10,7 @@ public class MySQLClass {
         tableMessageCreate();
         tableAuthorizationCreate();
         rooms();
+        tableUserInRoom();
     }
 
     public Connection getConnection(String dbName) throws SQLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
@@ -127,6 +128,74 @@ public class MySQLClass {
         }
     }
 
+    public void tableUserInRoom(){
+        try{
+            Connection conn = null;
+            Statement st = null;
+            try{
+                conn = getConnection("chat");
+                st = conn.createStatement();
+                st.executeUpdate("CREATE TABLE IF NOT EXISTS chat.userInRoom " +
+                        "(lustTimeClientUpdate BIGINT NOT NULL, roomId INT NOT NULL, " +
+                        "userId INT NOT NULL, userName VARCHAR(20) NOT NULL)");
+            }
+            finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(st != null){
+                        st.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void tablePrivateMessages(){
+        try{
+            Connection conn = null;
+            Statement st = null;
+            try{
+                conn = getConnection("chat");
+                st = conn.createStatement();
+                st.executeUpdate("CREATE TABLE IF NOT EXISTS chat.privateMessages " +
+                        "(lustTimeClientUpdate BIGINT NOT NULL, message VARCHAR(20) NOT NULL, " +
+                        "userSender VARCHAR(20) NOT NULL, userTaker VARCHAR(20) NOT NULL)");
+            }
+            finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(st != null){
+                        st.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public void tableAuthorizationCreate(){
         try{
             Connection conn = null;
@@ -161,17 +230,18 @@ public class MySQLClass {
         }
     }
 
-    public void addMessage(Integer room, Integer id, String message, String user){
+    public void addUserInRoom(UserInRoom userInRoom){
         try{
             Connection conn = null;
             PreparedStatement ps = null;
 
             try{
                 conn = getConnection("chat");
-                ps = conn.prepareStatement("INSERT INTO room" + room + " (id, message, user) VALUES (?, ?, ?)");
-                ps.setInt(1, id);
-                ps.setString(2, message);
-                ps.setString(3, user);
+                ps = conn.prepareStatement("INSERT INTO userInRoom (lustTimeClientUpdate, roomId, userId, userName) VALUES (?, ?, ?, ?)");
+                ps.setLong(1, userInRoom.getLustTimeClientUpdate());
+                ps.setInt(2, userInRoom.getRoomId());
+                ps.setInt(3, userInRoom.getUserId());
+                ps.setString(4, userInRoom.getUserName());
                 ps.executeUpdate();
             } finally {
                 try{
@@ -194,7 +264,79 @@ public class MySQLClass {
         }
     }
 
-    public void addAuthorization(Integer id, User user){
+    public void addPrivateMessage(String message, String userSender, String userTaker){
+        try{
+            Connection conn = null;
+            PreparedStatement ps = null;
+
+            try{
+                conn = getConnection("chat");
+                ps = conn.prepareStatement("INSERT INTO privateMessages (lustTimeClientUpdate, message, userSender, userTaker) VALUES (?, ?, ?, ?)");
+                ps.setLong(1, System.currentTimeMillis());
+                ps.setString(2, message);
+                ps.setString(3, userSender);
+                ps.setString(4, userTaker);
+
+                ps.executeUpdate();
+            } finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(ps != null){
+                        ps.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Message addMessage(Integer room, Integer id, String message, String user){
+        Message m = null;
+        try{
+            Connection conn = null;
+            PreparedStatement ps = null;
+
+            try{
+                conn = getConnection("chat");
+                ps = conn.prepareStatement("INSERT INTO room" + room + " (id, message, user) VALUES (?, ?, ?)");
+                ps.setInt(1, id);
+                ps.setString(2, message);
+                ps.setString(3, user);
+                m = new Message(id, message, user);
+
+                ps.executeUpdate();
+            } finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(ps != null){
+                        ps.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return m;
+    }
+
+    public void addAuthorization(User user){
         try{
             Connection conn = null;
             PreparedStatement ps = null;
@@ -202,7 +344,7 @@ public class MySQLClass {
             try{
                 conn = getConnection("chat");
                 ps = conn.prepareStatement("INSERT INTO authorization (id, login, password) VALUES (?, ?, ?)");
-                ps.setInt(1, id);
+                ps.setInt(1, user.getId());
                 ps.setString(2, user.getUserName());
                 ps.setString(3, user.getUserPassword());
                 ps.executeUpdate();
@@ -225,6 +367,92 @@ public class MySQLClass {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void deleteUserInRoom(int userId){
+        try{
+            Connection conn = null;
+            PreparedStatement ps = null;
+
+            try{
+                conn = getConnection("chat");
+                ps = conn.prepareStatement("DELETE FROM userInRoom WHERE userId = ?");
+                ps.setInt(1, userId);
+                ps.executeUpdate();
+            } finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(ps != null){
+                        ps.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> checkAuthorization (){
+        List<User> list = new LinkedList<>();
+
+        try{
+            Connection conn = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+
+            try{
+                conn = getConnection("chat");
+                String query = "SELECT * FROM authorization";
+                ps = conn.prepareStatement(query);
+                rs = ps.executeQuery();
+
+                while (rs.next()){
+                    try{
+                        int id = rs.getInt("id");
+                        String login = rs.getString("login");
+                        String password = rs.getString("password");
+                        User user = new User(id, login, password);
+                        list.add(user);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            } finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(ps != null){
+                        ps.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(rs != null){
+                        rs.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
     }
 
     public Map<String, String> check (){
@@ -279,6 +507,119 @@ public class MySQLClass {
         }
 
         return map;
+    }
+
+    public Map<Integer, Map<Integer, String>> checkUsersList(){
+        Map<Integer, Map<Integer, String>> returnMap = new HashMap<>();
+        Map<Integer, String> map = new HashMap<>();
+
+        try{
+            Connection conn = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+
+            try{
+                conn = getConnection("chat");
+                String query = "SELECT * FROM userInRoom";
+                ps = conn.prepareStatement(query);
+                rs = ps.executeQuery();
+
+                while (rs.next()){
+                    try{
+                        int roomId = rs.getInt("roomId");
+                        int userId = rs.getInt("userId");
+                        String userName = rs.getString("userName");
+
+                        map.put(userId, userName);
+                        returnMap.put(roomId, map);
+
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            } finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(ps != null){
+                        ps.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(rs != null){
+                        rs.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return returnMap;
+    }
+
+    public List<UserInRoom> checkUserInRoom(){
+        List<UserInRoom> list = new LinkedList<>();
+
+        try{
+            Connection conn = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+
+            try{
+                conn = getConnection("chat");
+                String query = "SELECT * FROM userInRoom";
+                ps = conn.prepareStatement(query);
+                rs = ps.executeQuery();
+
+                while (rs.next()){
+                    try{
+                        long lustTimeClientUpdate = rs.getLong("lustTimeClientUpdate");
+                        int roomId = rs.getInt("roomId");
+                        int userId = rs.getInt("userId");
+                        String userName = rs.getString("userName");
+                        UserInRoom u = new UserInRoom(lustTimeClientUpdate, roomId, userId, userName);
+                        list.add(u);
+
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            } finally {
+                try{
+                    if(conn != null){
+                        conn.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(ps != null){
+                        ps.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                try{
+                    if(rs != null){
+                        rs.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public List<Message> checkMessage (Integer room){
@@ -338,5 +679,4 @@ public class MySQLClass {
         return list;
 
     }
-
 }
